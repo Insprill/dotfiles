1e5d503c171e98470401b767b00b75738667d85c
Pierce Thompson <insprill@gmail.com>
2023-06-11 10:53:08 -0400

    Remove Mason bini from Neovim path (fixed in NvChad)

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -10,7 +10,3 @@ opt.relativenumber = true
 vim.cmd([[
   autocmd FocusGained,BufEnter * checktime
 ]])
-
--- Add binaries installed by mason.nvim to path (see https://github.com/williamboman/mason.nvim/issues/1289#issuecomment-1549875448)
-local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
-vim.env.PATH = vim.fn.stdpath "data" .. "/mason/bin" .. (is_windows and ";" or ":") .. vim.env.PATH
