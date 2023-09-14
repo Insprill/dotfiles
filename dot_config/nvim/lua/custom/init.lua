8aa5e2e3d9152bfcb8a88a4f9de0c1e49377b109
Pierce Thompson <insprill@gmail.com>
2023-09-14 19:43:49 -0400

    Add Neovide settings

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -10,3 +10,9 @@ opt.relativenumber = true
 vim.cmd([[
   autocmd FocusGained,BufEnter * checktime
 ]])
+
+-- Neovide
+vim.o.guifont = "MesloLGS Nerd Font"
+vim.g.neovide_scale_factor = 0.8
+vim.g.neovide_cursor_animation_length = 0.05
+vim.g.neovide_cursor_trail_size = 0.5
