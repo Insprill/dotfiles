6c7e2b95e45551802c4c669cee28c39be8b21a9f
Pierce Thompson <pierce@insprill.net>
2024-11-02 15:24:18 -0400

    Move Neovim compiler hack-fix to options.lua

--- dot_config/nvim/lua/chadrc.lua
+++ dot_config/nvim/lua/chadrc.lua
@@ -14,16 +14,4 @@ M.base46 = {
   -- },
 }
 
--- This is super hacky, but necessary for nvim-treesitter to work
--- properly on Windows. Using CC or GCC will cause Neovim to crash
--- as soon as a treesitter is loaded. Using zig causes nothing to
--- compile at all. Setting "require 'nvim-treesitter.install'.compilers"
--- to "zig" in the plugins' init function works, but not for the first
--- install, since the compilation process starts before init is called.
--- This ends up being the only way to get everything fully functional
--- the first time nvim-treesitter is installed.
-if jit.os == "Windows" then
-  vim.fn.setenv('CC', 'clang')
-end
-
 return M
