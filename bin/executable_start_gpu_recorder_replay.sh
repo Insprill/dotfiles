7bdb15c6a41fee07b2432512a52ad02492624332
Pierce Thompson <pierce@insprill.net>
2024-12-06 19:42:46 -0500

    Update gpu screen recording script

--- bin/executable_start_gpu_recorder_replay.sh
+++ bin/executable_start_gpu_recorder_replay.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gpu-screen-recorder -w DP-2 -c mp4 -k av1 -f 60 -r 120 -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -o /home/insprill/Videos/
+gpu-screen-recorder -w DP-2 -c mp4 -k av1 -f 60 -r 120 -a "default_input|default_output" -o "$HOME/Videos"
