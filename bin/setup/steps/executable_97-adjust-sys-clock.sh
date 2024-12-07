c10c52750114162eb35c96f4983a87d597ea2e50
Pierce Thompson <pierce@insprill.net>
2024-12-06 19:57:54 -0500

    Remove duplicate Linux setup step

--- bin/setup/steps/executable_97-adjust-sys-clock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set the system clock to local time instead of UTC.
-# This fixes time issues when dual booting Windows, since
-# it always expects sys time to be local.
-if ! timedatectl set-local-rtc 1 --adjust-system-clock; then
-    echo "Error: Failed to set system clock to local time! If you're dual booting windows, expect time issues!"
-fi
