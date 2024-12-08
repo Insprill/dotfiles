14753431e1ccd9237c3bedffb471db8ce04a2e05
Pierce Thompson <pierce@insprill.net>
2024-12-08 05:19:07 -0500

    Make gpu screen recorder script auto-select the best codec based on capabilities

--- bin/executable_start_gpu_recorder_replay.sh
+++ bin/executable_start_gpu_recorder_replay.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-gpu-screen-recorder -w "$DEFAULT_MONITOR" -c mp4 -k av1 -f 60 -r 120 -a "default_input|default_output" -o "$HOME/Videos"
+
+if vulkaninfo | grep -q -i 'VK_KHR_video_encode_av1'; then
+    codec="av1_10bit"
+elif vulkaninfo | grep -q -i 'VK_KHR_video_encode_h265'; then
+    codec="hevc_10bit"
+else
+    codec="h264"
+fi
+
+gpu-screen-recorder -w "$DEFAULT_MONITOR" -c mp4 -k "$codec" -f 60 -r 120 -a "default_input|default_output" -o "$HOME/Videos"
