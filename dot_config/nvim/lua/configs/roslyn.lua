68131f953440b41e13578746e62d5e2baff8ddd0
Pierce Thompson <pierce@insprill.net>
2025-02-13 13:36:33 -0500

    Update nvim roslyn config

--- dot_config/nvim/lua/configs/roslyn.lua
+++ dot_config/nvim/lua/configs/roslyn.lua
@@ -1,1 +1,38 @@
-return {}
+return {
+  config = {
+    settings = {
+      ["csharp|background_analysis"] = {
+        background_analysis = {
+          dotnet_analyzer_diagnostics_scope = "fullSolution",
+          dotnet_compiler_diagnostics_scope = "fullSolution",
+        },
+      },
+    },
+  },
+
+  capabilities = function()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    -- Extend existing capabilities without overwriting them
+    capabilities = vim.tbl_deep_extend("force", capabilities, {
+      textDocument = {
+        foldingRange = {
+          dynamicRegistration = true,
+          lineFoldingOnly = true,
+        },
+        completion = {
+          completionItem = {
+            snippetSupport = true,
+          },
+        },
+      },
+      workspace = {
+        didChangeWatchedFiles = {
+          dynamicRegistration = true,
+        },
+      },
+    })
+
+    return capabilities
+  end,
+}
