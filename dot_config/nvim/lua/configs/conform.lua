4092168cbc7d14c1527b9025f5dc3c941f2be714
Pierce Thompson <pierce@insprill.net>
2025-02-13 02:45:20 -0500

    Set conform to fallback to LSP formatter

--- dot_config/nvim/lua/configs/conform.lua
+++ dot_config/nvim/lua/configs/conform.lua
@@ -24,6 +24,10 @@ return {
     markdown = { "mdformat" },
   },
 
+  default_format_opts = {
+    lsp_format = "fallback",
+  },
+
   -- format_on_save = {
   --   timeout_ms = 500,
   --   lsp_fallback = true,
