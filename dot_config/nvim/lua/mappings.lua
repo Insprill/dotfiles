49277cd6ec92e01e493736807bfba1e77c254c7e
Pierce Thompson <pierce@insprill.net>
2025-04-26 13:54:01 -0400

    Add a Neovim bind to toggle between virtual text and virtual lines

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -1,18 +1,13 @@
 require "nvchad.mappings"
 
--- disabled mappings
-local nomap = vim.keymap.del
--- nomap("i", "<TAB>")
-
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
 map("n", "<leader>km", ":Telescope keymaps<CR>")
--- map("i", "<leader>pa", 'copilot#Accept("\\<CR>")', {
---   expr = true,
---   replace_keycodes = false,
--- })
--- vim.g.copilot_no_tab_map = true
+map("n", "<leader>dt", function()
+  local cfg = vim.diagnostic.config()
+  vim.diagnostic.config {
+    virtual_text = not cfg.virtual_text,
+    virtual_lines = cfg.virtual_text,
+  }
+end, { desc = "Toggle diagnostics: virtual text ↔ virtual lines" })
