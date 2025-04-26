92506490abab0021246f14f1dfcba1be51bf3eac
Pierce Thompson <pierce@insprill.net>
2025-04-25 23:20:38 -0400

    Simplify Neovim lsp initialization
    https://github.com/NvChad/starter/commit/2ef0168470ad6d1bc68e44177fa05b43110d9e25

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -30,13 +30,7 @@ return function()
   }
 
   -- lsps with default config
-  for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
-      on_attach = nvlsp.on_attach,
-      on_init = nvlsp.on_init,
-      capabilities = nvlsp.capabilities,
-    }
-  end
+  vim.lsp.enable(servers)
 
   -- clangd
   lspconfig.clangd.setup {
@@ -63,9 +57,6 @@ return function()
   lspconfig.rust_analyzer.setup {
     settings = {
       ["rust-analyzer"] = {
-        diagnostics = {
-          enable = false,
-        },
         cargo = {
           allFeatures = true,
         },
