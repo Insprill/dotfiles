2c822157331a1d3b5e43ffa27d85f30a27221070
Pierce Thompson <pierce@insprill.net>
2025-04-26 18:11:24 -0400

    Replace <leader>ds bind with Telescope diagnostics

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -3,7 +3,9 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+
 map("n", "<leader>km", ":Telescope keymaps<CR>")
+map("n", "<leader>ds", ":Telescope diagnostics<CR>")
 map("n", "<leader>dt", function()
   local cfg = vim.diagnostic.config()
   vim.diagnostic.config {
