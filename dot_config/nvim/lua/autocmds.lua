67f09545ed55e69b9565736a7c8fe6e58e301ca3
Pierce Thompson <pierce@insprill.net>
2025-04-26 03:24:33 -0400

    Restore Neovim cursor position

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -1,9 +1,29 @@
+local autocmd = vim.api.nvim_create_autocmd
+
 -- Show Nvdash when all buffers are closed
-vim.api.nvim_create_autocmd("BufDelete", {
+-- https://nvchad.com/docs/recipes#show_nvdash_when_all_buffers_are_closed
+autocmd("BufDelete", {
   callback = function()
     local bufs = vim.t.bufs
     if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
       vim.cmd "Nvdash"
+    end
+  end,
+})
+
+-- Restore cursor position
+-- https://nvchad.com/docs/recipes#restore_cursor_position
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
     end
   end,
 })
