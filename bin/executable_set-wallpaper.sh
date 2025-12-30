4745a0ebb9296f3b92d4418cf4853a104b2488af
Pierce Thompson <pierce@insprill.net>
2025-12-30 04:38:52 -0500

    Update set-wallpaper script
    Wtf is this new IPC? Per-monitor only and no way to get the currently
    loaded wallpapers.

--- bin/executable_set-wallpaper.sh
+++ bin/executable_set-wallpaper.sh
@@ -3,14 +3,16 @@
 # https://github.com/hyprwm/hyprpaper/issues/194#issuecomment-2632115950
 
 WALLPAPER_DIR="$HOME/Pictures/wallpapers/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 # Get a random wallpaper that is not the current one (Insprill: limited to image files so Nextcloud sync files don't crash hyprpaper 💀)
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( \
     -iname "*.png" -o \
     -iname "*.jpg" -o \
     -iname "*.jpeg" \
-\) ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+\) | shuf -n 1)
 
 # Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
+for i in $(hyprctl monitors | awk '/^Monitor / {print $2}');
+do
+    hyprctl hyprpaper wallpaper $i,"$WALLPAPER"
+done
