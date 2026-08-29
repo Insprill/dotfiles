9b545722565ab7c13be911269023aeb893082dba
Pierce Thompson <pierce@insprill.net>
2026-08-29 02:34:49 -0400

    Add Bitwarden windowrules

---
+++ dot_config/hypr/apps/bitwarden.lua
@@ -0,0 +1,9 @@
+hl.window_rule({
+	name = "Bitwarden",
+	match = {
+		class = "^(\\w*-nngceckbapebfimnlniiiahkandclblb-Default)$",
+		title = "^(Bitwarden)$",
+	},
+	no_screen_share = true,
+	float = true,
+})
