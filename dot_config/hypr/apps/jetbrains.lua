cbe79fa54a30dbe5bb732edfd2c43e6c104483fc
Pierce Thompson <pierce@insprill.net>
2026-09-01 02:53:15 -0400

    Update Jetbrains windowrules

--- dot_config/hypr/apps/jetbrains.lua
+++ dot_config/hypr/apps/jetbrains.lua
@@ -1,6 +1,8 @@
-hl.window_rule({ match = { title = "^(Welcome to JetBrains Rider)$" }, float = true })
-hl.window_rule({ match = { title = "^(Welcome to IntelliJ IDEA)$" }, float = true })
-hl.window_rule({ match = { class = "^(jetbrains-idea)$" }, opacity = 0.95 })
-hl.window_rule({ match = { class = "^(jetbrains-rider)$" }, opacity = 0.95 })
--- Disable mouse focus (see https://github.com/basecamp/omarchy/pull/5183#issuecomment-4189299971).
-hl.window_rule({ match = { class = "^(jetbrains-.*)$" }, no_follow_mouse = true })
+hl.window_rule({
+	match = {
+		class = "^(jetbrains-.*)$",
+		title = "^(Welcome to .*)$",
+	},
+	float = true,
+})
+hl.window_rule({ match = { class = "^(jetbrains-.*)$" }, opacity = 0.95 })
